a9f8d9057811c2ff84ce188eb0205a3ec07145fe
Bukola Eunice <thelmabukky87@gmail.com>
2022-11-23 22:09:48 +0100

    Intro to music page

---
+++ lib/screens/music/intro_music.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IntroMusic extends StatelessWidget {
+  const IntroMusic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
